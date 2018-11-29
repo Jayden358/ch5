9249cb93649cd15cc004950f01da65948be97085
jayden.williams <jayden.williams@CLC-LAB209-17.TCSD.tooelesd.org>
2018-11-29 13:24:10 -0700

    a

---
+++ Ch5/src/jaydenChapter5/ASCIIcodeDisplay.java
@@ -0,0 +1,275 @@
+package jaydenChapter5;
+
+import java.util.Scanner;
+
+public class ASCIIcodeDisplay {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+	
+		
+	     
+	   int number = input.nextInt();
+	     
+	     if (number==0) {
+	    	 System.out.println("NUL");
+	     }
+	     else  if (number==1) {
+	    	 System.out.println("SOH");
+	     }   else if (number==2) {
+	    	 System.out.println("STX");
+	     }  else  if (number==3) {
+	    	 System.out.println("ETX");
+	     }  else  if (number==4) {
+	    	 System.out.println("EOT");
+	     }  else  if (number==5) {
+	    	 System.out.println("ENQ");
+	     }  else  if (number==6) {
+	    	 System.out.println("ACK");
+	     }   else if (number==7) {
+	    	 System.out.println("BEL");
+	     }   else if (number==8) {
+	    	 System.out.println("BS");
+	     }   else if (number==9) {
+	    	 System.out.println("TAB");
+	     }   else if (number==10) {
+	    	 System.out.println("LF");
+	     }  else  if (number==11) {
+	    	 System.out.println("VT");
+	     }   else if (number==12) {
+	    	 System.out.println("FF");
+	     }   else if (number==13) {
+	    	 System.out.println("CR");
+	     }   else if (number==14) {
+	    	 System.out.println("SO");
+	     }   else if (number==15) {
+	    	 System.out.println("SI");
+	     }   else if (number==16) {
+	    	 System.out.println("DLE");
+	     }   else if (number==17) {
+	    	 System.out.println("DC1");
+	     }   else if (number==18) {
+	    	 System.out.println("DC2");
+	     }   else if (number==19) {
+	    	 System.out.println("DC3");
+	     }  else  if (number==20) {
+	    	 System.out.println("DC4");
+	     }   else if (number==21) {
+	    	 System.out.println("NAK");
+	     }   else if (number==22) {
+	    	 System.out.println("SYN");
+	     }   else if (number==23) {
+	    	 System.out.println("ETB");
+	     }   else if (number==24) {
+	    	 System.out.println("CAN");
+	     }   else if (number==25) {
+	    	 System.out.println("EM");
+	     }   else if (number==26) {
+	    	 System.out.println("SUB");
+	     }  else  if (number==27) {
+	    	 System.out.println("ESC");
+	     }   else if (number==28) {
+	    	 System.out.println("FS");
+	     }   else if (number==29) {
+	    	 System.out.println("GS");
+	     }   else if (number==30) {
+	    	 System.out.println("RS");
+	     }   else if (number==31) {
+	    	 System.out.println("US");
+	     }   else if (number==32) {
+	    	 System.out.println("SPACE");
+	     }   else if (number==33) {
+	    	 System.out.println("!");
+	     }   else if (number==34) {
+	    	 System.out.println("\"");
+	     }   else if (number==35) {
+	    	 System.out.println("#");
+	     }   else if (number==36) {
+	    	 System.out.println("$");
+	     }   else if (number==37) {
+	    	 System.out.println("%");
+	     }  else  if (number==38) {
+	    	 System.out.println("&");
+	     }   else if (number==39) {
+	    	 System.out.println("'");
+	     }   else if (number==40) {
+	    	 System.out.println("(");
+	     }   else if (number==41) {
+	    	 System.out.println(")");
+	     }   else if (number==42) {
+	    	 System.out.println("*");
+	     }   else if (number==43) {
+	    	 System.out.println("+");
+	     }  else  if (number==44) {
+	    	 System.out.println(",");
+	     }   else if (number==45) {
+	    	 System.out.println("-");
+	     }   else if (number==46) {
+	    	 System.out.println(".");
+	     }  else  if (number==47) {
+	    	 System.out.println("/");
+	     }  else  if (number==48) {
+	    	 System.out.println("0");
+	     }  else  if (number==49) {
+	    	 System.out.println("1");
+	     }  else  if (number==50) {
+	    	 System.out.println("2");
+	     }   else if (number==51) {
+	    	 System.out.println("3");
+	     }   else if (number==52) {
+	    	 System.out.println("4");
+	     }   else if (number==53) {
+	    	 System.out.println("5");
+	     }   else if (number==54) {
+	    	 System.out.println("6");
+	     }  else  if (number==55) {
+	    	 System.out.println("7");
+	     }   else if (number==56) {
+	    	 System.out.println("8");
+	     }   else if (number==57) {
+	    	 System.out.println("9");
+	     }   else if (number==58) {
+	    	 System.out.println(":");
+	     }   else if (number==59) {
+	    	 System.out.println(";");
+	     }   else if (number==60) {
+	    	 System.out.println("<");
+	     }   else if (number==61) {
+	    	 System.out.println("=");
+	     }   else if (number==62) {
+	    	 System.out.println(">");
+	     }   else if (number==63) {
+	    	 System.out.println("?");
+	     }  else  if (number==64) {
+	    	 System.out.println("@");
+	     }   else if (number==65) {
+	    	 System.out.println("A");
+	     }   else if (number==66) {
+	    	 System.out.println("B");
+	     }   else if (number==67) {
+	    	 System.out.println("C");
+	     }   else if (number==68) {
+	    	 System.out.println("D");
+	     }   else if (number==69) {
+	    	 System.out.println("E");
+	     }   else if (number==70) {
+	    	 System.out.println("F");
+	     }  else  if (number==71) {
+	    	 System.out.println("G");
+	     }   else if (number==72) {
+	    	 System.out.println("H");
+	     }   else if (number==73) {
+	    	 System.out.println("I");
+	     }   else if (number==74) {
+	    	 System.out.println("J");
+	     }   else if (number==75) {
+	    	 System.out.println("K");
+	     }   else if (number==76) {
+	    	 System.out.println("L");
+	     }   else if (number==77) {
+	    	 System.out.println("M");
+	     }   else if (number==78) {
+	    	 System.out.println("N");
+	     }   else if (number==79) {
+	    	 System.out.println("O");
+	     }   else if (number==80) {
+	    	 System.out.println("P");
+	     }   else if (number==81) {
+	    	 System.out.println("Q");
+	     }  else  if (number==82) {
+	    	 System.out.println("R");
+	     }   else if (number==83) {
+	    	 System.out.println("S");
+	     }   else if (number==84) {
+	    	 System.out.println("T");
+	     }   else if (number==85) {
+	    	 System.out.println("U");
+	     }   else if (number==86) {
+	    	 System.out.println("V");
+	     }   else if (number==87) {
+	    	 System.out.println("W");
+	     }   else if (number==88) {
+	    	 System.out.println("X");
+	     }  else  if (number==89) {
+	    	 System.out.println("Y");
+	     }   else if (number==90) {
+	    	 System.out.println("Z");
+	     }   else if (number==91) {
+	    	 System.out.println("[");
+	     }  else  if (number==92) {
+	    	 System.out.println("\\");
+	     }  else  if (number==93) {
+	    	 System.out.println("]");
+	     }  else  if (number==94) {
+	    	 System.out.println("^");
+	     }  else  if (number==95) {
+	    	 System.out.println("_");
+	     }   else if (number==96) {
+	    	 System.out.println("`");
+	     }   else if (number==97) {
+	    	 System.out.println("a");
+	     }   else if (number==98) {
+	    	 System.out.println("b");
+	     }   else if (number==99) {
+	    	 System.out.println("c");
+	     }  else  if (number==100) {
+	    	 System.out.println("d");
+	     }   else if (number==101) {
+	    	 System.out.println("e");
+	     }   else if (number==102) {
+	    	 System.out.println("f");
+	     }   else if (number==103) {
+	    	 System.out.println("g");
+	     }   else if (number==104) {
+	    	 System.out.println("h");
+	     }   else if (number==105) {
+	    	 System.out.println("i");
+	     }   else if (number==106) {
+	    	 System.out.println("j");
+	     }   else if (number==107) {
+	    	 System.out.println("k");
+	     }   else if (number==108) {
+	    	 System.out.println("l");
+	     }  else  if (number==109) {
+	    	 System.out.println("m");
+	     }   else if (number==110) {
+	    	 System.out.println("n");
+	     }   else if (number==111) {
+	    	 System.out.println("o");
+	     }   else if (number==112) {
+	    	 System.out.println("p");
+	     }   else if (number==113) {
+	    	 System.out.println("q");
+	     }   else if (number==114) {
+	    	 System.out.println("r");
+	     }   else if (number==115) {
+	    	 System.out.println("s");
+	     }  else  if (number==116) {
+	    	 System.out.println("t");
+	     }   else if (number==117) {
+	    	 System.out.println("u");
+	     }   else if (number==118) {
+	    	 System.out.println("v");
+	     }   else if (number==119) {
+	    	 System.out.println("w");
+	     }   else if (number==120) {
+	    	 System.out.println("x");
+	     }   else if (number==121) {
+	    	 System.out.println("y");
+	     }   else if (number==122) {
+	    	 System.out.println("z");
+	     }   else if (number==123) {
+	    	 System.out.println("{");
+	     }   else if (number==124) {
+	    	 System.out.println("|");
+	     }   else if (number==125) {
+	    	 System.out.println("}");
+	     }   else if (number==126) {
+	    	 System.out.println("~");
+	     }  else  if (number==127) {
+	    	 System.out.println("DEL");
+	     
+	    
+	}
+	}
+}
---
+++ Ch5/src/jaydenChapter5/PrintFStatements.java
@@ -0,0 +1,24 @@
+package jaydenChapter5;
+
+public class PrintFStatements {
+
+	public static void main(String[] args) {
+		/*
+		double amount = 12618.98;
+		double interestRate = 0.0013;
+		double interest = amount * interestRate;
+		System.out.printf("Interest is $%4.2f", interest);
+	*/
+	/*	int count = 5;
+		double amount = 45.56;
+	System.out.printf("count is %d and amount is %f", count,amount);
+	*/
+		
+		/* String word= "hello world";
+		System.out.printf("%s", word);
+		*/
+		System.out.printf("%3d#\n%2s#\n%4.2f\n", 1234,"java", 51.6653);
+	}
+	
+
+}
